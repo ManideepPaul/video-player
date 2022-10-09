1440b12c8d53192d0efcedeb89f776a1a74e9833
manideep <17101992m@gmail.com>
2022-10-09 23:28:42 +0530

    video changing method changed

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ const video = document.querySelector("video");
 const forward = document.querySelector(".forward");
 const backward = document.querySelector(".backward");
 
-prev.addEventListener('click', changeVideo);
-next.addEventListener('click', changeVideo);
+prev.addEventListener('click', () => changeVideo("prev"));
+next.addEventListener('click', () => changeVideo("next"));
+
+video.src = videoArr[0];
+let videoNum = 0;
 
 forward.addEventListener('click', () => {
     video.currentTime += 5;
@@ -17,6 +20,20 @@ backward.addEventListener('click', () => {
     video.currentTime -= 5;
 })
 
-function changeVideo() {
-    video.src = video.src.includes("Campfire") ? videoArr[1] : videoArr[0];
+function changeVideo(value) {
+    console.log(value, videoNum)
+    if(videoNum === 0 && value === "prev"){
+        video.src = videoArr[videoNum];
+    }
+    else if(videoNum === videoArr.length - 1 && value === "next") {
+        video.src = videoArr[videoNum];
+    }
+    else if(value === "next") {
+        videoNum++;
+        video.src = videoArr[videoNum]
+    }
+    else {
+        videoNum--;
+        video.src = videoArr[videoNum]
+    }
 }
